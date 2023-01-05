22adee04a5e9ceada65fe2b301ada422534dee74
Lev Yuvensky <levayu22@gmail.com>
2023-01-05 23:46:45 +0300

    Refactor UserRepository

--- internal/repositories/postgresql.go
+++ internal/repositories/postgresql.go
@@ -5,17 +5,17 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type UserRepository struct {
+type userRepository struct {
 	postgresClient *pgxpool.Pool
 }
 
-func NewUserRepository(pc *pgxpool.Pool) *UserRepository {
-	return &UserRepository{
+func NewPostgresUserRepository(pc *pgxpool.Pool) UserRepository {
+	return &userRepository{
 		postgresClient: pc,
 	}
 }
 
-func (ur *UserRepository) CreateUser(ctx context.Context, username string) (userID int, err error) {
+func (ur *userRepository) CreateUser(ctx context.Context, username string) (userID int, err error) {
 	q := `
 		INSERT INTO users (username)
 		VALUES ($1)
