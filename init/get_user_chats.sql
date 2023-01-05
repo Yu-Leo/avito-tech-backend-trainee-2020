bf9bfcc7ec024e275b8c00b45eb6956c50a080a5
Lev Yuvensky <levayu22@gmail.com>
2023-01-05 21:29:31 +0300

    Upd get_user_chats.sql

--- init/get_user_chats.sql
+++ init/get_user_chats.sql
@@ -1,3 +1,7 @@
 SELECT *
 FROM users_chats
-WHERE users_chats.user_id = 1;
+WHERE users_chats.user_id = 1
+ORDER BY (SELECT (MAX(created_at))
+          FROM messages
+          WHERE chat_id = users_chats.chat_id
+          GROUP BY chat_id) DESC;
