94016d158c826cd3da535e17143a0aa3104ba0ca
Lev Yuvenskiy <levayu22@gmail.com>
2023-01-05 22:51:03 +0300

    Add entities

---
+++ internal/entities/entities.go
@@ -0,0 +1,24 @@
+package entities
+
+import "time"
+
+type User struct {
+	Id        int       `json:"id"`
+	Username  string    `json:"username"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type Chat struct {
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	Users     []User    `json:"users"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type Message struct {
+	Id        int       `json:"id"`
+	ChatId    int       `json:"chat"`
+	UserId    int       `json:"author"`
+	Text      int       `json:"text"`
+	CreatedAt time.Time `json:"createdAt"`
+}
