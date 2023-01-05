ca1262059f31403fab03e77085d29264c5dbbea5
Lev Yuvenskiy <levayu22@gmail.com>
2023-01-05 21:53:15 +0300

    Add PostgreSQL client

---
+++ pkg/postgresql/postgresql.go
@@ -0,0 +1,55 @@
+package postgresql
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/Yu-Leo/avito-tech-backend-trainee-2020/config"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"time"
+)
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+	BeginTxFunc(ctx context.Context, txOptions pgx.TxOptions, f func(pgx.Tx) error) error
+}
+
+func NewClient(ctx context.Context, maxAttempts int, sc config.StorageConfig) (pool *pgxpool.Pool, err error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s", sc.Username, sc.Password, sc.Host, sc.Port, sc.Database)
+	err = doWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		pool, err = pgxpool.New(ctx, dsn)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, maxAttempts, 5*time.Second)
+
+	if err != nil {
+		log.Fatal("error do with tries postgresql")
+	}
+
+	return pool, nil
+}
+
+func doWithTries(fn func() error, attempts int, delay time.Duration) (err error) {
+	for attempts > 0 {
+		if err = fn(); err != nil {
+			time.Sleep(delay)
+			attempts--
+			continue
+		}
+		return nil
+	}
+	return errors.New("error")
+}
