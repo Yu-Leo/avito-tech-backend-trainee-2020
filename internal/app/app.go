53ee66c3eaaf502e5c8bf1122574b88856681051
Lev Yuvenskiy <levayu22@gmail.com>
2023-01-06 13:05:38 +0300

    Add loger

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,15 +7,23 @@ import (
 	"github.com/Yu-Leo/avito-tech-backend-trainee-2020/internal/repositories"
 	"github.com/Yu-Leo/avito-tech-backend-trainee-2020/internal/usecases"
 	"github.com/Yu-Leo/avito-tech-backend-trainee-2020/pkg/httpserver"
+	"github.com/Yu-Leo/avito-tech-backend-trainee-2020/pkg/logger"
 	"github.com/Yu-Leo/avito-tech-backend-trainee-2020/pkg/postgresql"
 	"github.com/gin-gonic/gin"
 )
 
 func Run(cfg *config.Config) {
-	postgresClient, err := postgresql.NewClient(context.TODO(), 3, cfg.Storage)
+	l := logger.NewLogger(cfg.Logger.Level)
+
+	l.Info("Run application")
+
+	postgresClient, err := postgresql.NewClient(context.TODO(), 1, cfg.Storage)
 	if err != nil {
-		panic(err)
+		l.Fatal(err.Error())
 	}
+
+	l.Debug("Connected to postgres")
+
 	defer postgresClient.Close()
 
 	userRepository := repositories.NewPostgresUserRepository(postgresClient)
@@ -24,7 +32,7 @@ func Run(cfg *config.Config) {
 
 	ginEngine := gin.Default()
 
-	v1.NewRouter(ginEngine, userUserCase)
+	v1.NewRouter(ginEngine, userUserCase, l)
 
 	httpserver.New(ginEngine, httpserver.Port(cfg.Server.Port))
 	ginEngine.Run()
