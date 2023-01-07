9e29d71381299026a4a98d1c2020c524efc3c936
Lev Yuvenskiy <levayu22@gmail.com>
2023-01-07 17:00:50 +0300

    Change pgx.Conn to pgxpool.Pool

--- pkg/postgresql/postgresql.go
+++ pkg/postgresql/postgresql.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Yu-Leo/avito-tech-backend-trainee-2020/config"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"time"
 )
 
@@ -13,7 +14,8 @@ type Connection interface {
 	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
-	Close(ctx context.Context) error
+	Close()
+	Begin(ctx context.Context) (pgx.Tx, error)
 }
 
 func NewConnection(ctx context.Context, maxAttempts int, sc config.StorageConfig) (conn Connection, err error) {
@@ -22,7 +24,7 @@ func NewConnection(ctx context.Context, maxAttempts int, sc config.StorageConfig
 	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s", sc.Username, sc.Password, sc.Host, sc.Port, sc.Database)
 
 	for i := 0; i < maxAttempts; i++ {
-		conn, err = pgx.Connect(ctx, dsn)
+		conn, err = pgxpool.New(ctx, dsn)
 		if err == nil {
 			return conn, err
 		}
