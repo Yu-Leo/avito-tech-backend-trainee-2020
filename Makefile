1f0b4fc5527dd4a452f5b5bd0b31b41782cbabde
Lev Yuvensky <levayu22@gmail.com>
2023-01-12 20:47:36 +0300

    Add endpoint /chats/get

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ run: ### Run docker-compose
 .PHONY: run
 
 run-postgres: ### Run docker-compose only with postgres
-	docker-compose up -d postgres
+	docker-compose up postgres
 .PHONY: run-postgres
 
 stop: ### Down docker-compose
--- sql/get_user_chats.sql
+++ sql/get_user_chats.sql
@@ -1,7 +1,13 @@
-SELECT *
+SELECT chats.id, chats.name, chats.created_at
 FROM users_chats
+JOIN chats on users_chats.chat_id = chats.id
 WHERE users_chats.user_id = 1
 ORDER BY (SELECT (MAX(created_at))
           FROM messages
           WHERE chat_id = users_chats.chat_id
           GROUP BY chat_id) DESC;
+
+
+SELECT users_chats.user_id
+FROM users_chats
+WHERE users_chats.chat_id = 1;
